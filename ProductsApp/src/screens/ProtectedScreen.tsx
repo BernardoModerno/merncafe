5fbe64a2034fab75e0fcc80b6e68c454dc5d5a18
BernardoModerno <bernardo.moderno@gmail.com>
2023-02-22 11:25:12 -0300

    Inicio de config de rotas

---
+++ ProductsApp/src/screens/ProtectedScreen.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ProtectedScreen = () => {
+    return (
+        <View>
+            <Text>ProtectedScreen</Text>
+        </View>
+    );
+};
+
+export default ProtectedScreen;
