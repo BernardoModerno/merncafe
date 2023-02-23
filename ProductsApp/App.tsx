35c36c3841b8b5b19c8822d37452b2aedf719855
BernardoModerno <bernardo.moderno@gmail.com>
2023-02-23 14:31:42 -0300

    Formulário e Navegação

--- ProductsApp/App.tsx
+++ ProductsApp/App.tsx
@@ -1,7 +1,7 @@
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import { Navigator } from "./src/navigator/Navigator";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Navigator } from './src/navigator/Navigator';
 
 const App = () => {
   return (
--- ProductsApp/src/screens/LoginScreen.tsx
+++ ProductsApp/src/screens/LoginScreen.tsx
@@ -1,74 +1,108 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable prettier/prettier */
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { Platform, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Background from '../components/Background';
 import WhiteLogo from '../components/WhiteLogo';
+import { useForm } from '../hooks/useForm';
 import { loginStyles } from '../theme/loginTheme';
 
-const LoginScreen = () => {
+interface Props extends StackScreenProps<any, any> {}
+
+const LoginScreen = ({navigation} : Props) => {
+
+  const { email, password, onChange } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const onLogin = () => {
+    console.log({email, password});
+    Keyboard.dismiss();
+    
+  }
+
   return (
     <>
       {/* Background */}
       <Background />
 
-      <View style={ loginStyles.formContainer }>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={ (Platform.OS === 'ios') ? 'padding' : 'height'}
+      >
 
-         {/* Keyboard avoid view */}
-         <WhiteLogo />
-         <Text style={ loginStyles.title }>Login</Text>
-   
-         <Text style={loginStyles.label}>Email:</Text>
-         <TextInput
-           placeholder="Entre com seu email:"
-           placeholderTextColor="rgba(255,255,255,0.4)"
-           keyboardType="email-address"
-           underlineColorAndroid="white"
-           style={[
-             loginStyles.inputField,
-             ( Platform.OS === 'ios' ) && loginStyles.inputFieldIOS
-           ]}
-           selectionColor="white"
-   
-           autoCapitalize="none"
-           autoCorrect={ false }
-         />
-   
-         <Text style={loginStyles.label}>Senha:</Text>
-         <TextInput
-           placeholder="Entre com sua senha:"
-           placeholderTextColor="rgba(255,255,255,0.4)"
-           underlineColorAndroid="white"
-           style={[
-             loginStyles.inputField,
-             (Platform.OS === 'ios') && loginStyles.inputFieldIOS
-           ]}
-           selectionColor="white"
-   
-           autoCapitalize="none"
-           autoCorrect={false}
-         />
-   
-         {/* Botao Login */}
-         <View style={ loginStyles.buttonContainer }>
-           <TouchableOpacity
-               activeOpacity={ 0.8 }
-               style={ loginStyles.button }
-           >
-               <Text style={ loginStyles.buttonText } >Login</Text>
-           </TouchableOpacity>
-         </View>
-   
-         {/* criar uma nova conta */}
-         <View style={loginStyles.newUserContainer}>
-           <TouchableOpacity
-             activeOpacity={0.8}
-             onPress={ () => console.log('press') }
-           >
-             <Text style={loginStyles.buttonText} >Nova Conta</Text>
-           </TouchableOpacity>
-         </View>
-      </View>
+        <View style={ loginStyles.formContainer }>
+  
+           {/* Keyboard avoid view */}
+           <WhiteLogo />
+           <Text style={ loginStyles.title }>Login</Text>
+     
+           <Text style={loginStyles.label}>Email:</Text>
+           <TextInput
+             placeholder="Entre com seu email:"
+             placeholderTextColor="rgba(255,255,255,0.4)"
+             keyboardType="email-address"
+             underlineColorAndroid="white"
+             style={[
+               loginStyles.inputField,
+               ( Platform.OS === 'ios' ) && loginStyles.inputFieldIOS
+             ]}
+             selectionColor="white"
+
+             onChangeText={ (value) => onChange(value, 'email') }
+             value={email}
+             onSubmitEditing={ onLogin }
+
+             autoCapitalize="none"
+             autoCorrect={ false }
+           />
+     
+           <Text style={loginStyles.label}>Senha:</Text>
+           <TextInput
+             placeholder="Entre com sua senha:"
+             placeholderTextColor="rgba(255,255,255,0.4)"
+             underlineColorAndroid="white"
+             secureTextEntry
+             style={[
+               loginStyles.inputField,
+               (Platform.OS === 'ios') && loginStyles.inputFieldIOS
+             ]}
+             selectionColor="white"
+
+            onChangeText={(value) => onChange(value, 'password')}
+            value={password}
+            onSubmitEditing={onLogin}
+     
+             autoCapitalize="none"
+             autoCorrect={false}
+           />
+     
+           {/* Botao Login */}
+           <View style={ loginStyles.buttonContainer }>
+             <TouchableOpacity
+                 activeOpacity={ 0.8 }
+                 style={ loginStyles.button }
+                 onPress={ onLogin }
+             >
+                 <Text style={ loginStyles.buttonText } >Login</Text>
+             </TouchableOpacity>
+           </View>
+     
+           {/* criar uma nova conta */}
+           <View style={loginStyles.newUserContainer}>
+             <TouchableOpacity
+               activeOpacity={0.8}
+               onPress={() => navigation.replace('RegisterScreen') }
+             >
+               <Text style={loginStyles.buttonText} >Nova Conta</Text>
+             </TouchableOpacity>
+           </View>
+        </View>
+      </KeyboardAvoidingView>
     </>
   );
 };
